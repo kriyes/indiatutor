dfeb5628f9ba3ba9399d6fad250b559767af9101
kriyes <kriyes@gmail.com>
2020-05-10 17:27:36 +0530

    start durga2

---
+++ durgaindiatutor/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for durgaindiatutor project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'durgaindiatutor.settings')
+
+application = get_wsgi_application()
